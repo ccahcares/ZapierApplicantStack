e5623191645c1ed203c4f7c589977b1a22dde0cc
Michael Mahoney <admin@careconnectionsathome.com>
2020-11-30 20:29:41 -0500

    latest

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-const authentication = require('./authentication');
-const newHireTrigger = require('./triggers/new_hire.js');
-const hireDetailsTrigger = require('./triggers/hire_details.js');
-const newJobsTrigger = require('./triggers/new_jobs.js');
-const jobDetailsTrigger = require('./triggers/job_details.js');
-const newCandidateTrigger = require('./triggers/new_candidate.js');
-const candidateDetailsTrigger = require('./triggers/candidate_details.js');
-const allHiresTrigger = require('./triggers/all_hires.js');
-const updateHireCreate = require('./creates/update_hire_stage.js');
-const findHireSearch = require('./searches/find_hire.js');
-const allHiresSearch = require('./searches/all_hires.js');
+global._babelPolyfill || require('babel-polyfill');
 
 module.exports = require('./lib');
-
-module.exports = {
-  version: require('./package.json').version,
-  platformVersion: require('zapier-platform-core').version,
-  authentication: authentication,
-  triggers: {
-    [newHireTrigger.key]: newHireTrigger,
-    [hireDetailsTrigger.key]: hireDetailsTrigger,
-    [newJobsTrigger.key]: newJobsTrigger,
-    [jobDetailsTrigger.key]: jobDetailsTrigger,
-    [newCandidateTrigger.key]: newCandidateTrigger,
-    [candidateDetailsTrigger.key]: candidateDetailsTrigger,
-    [allHiresTrigger.key]: allHiresTrigger,
-  },
-  searches: {
-    [findHireSearch.key]: findHireSearch,
-    [allHiresSearch.key]: allHiresSearch,
-  },
-  creates: { [updateHireCreate.key]: updateHireCreate },
-};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import Authentication from './authentication';
+import newHireTrigger from './triggers/new_hire';
+import hireDetailsTrigger from './triggers/hire_details';
+import newJobsTrigger from './triggers/new_jobs.';
+import jobDetailsTrigger from './triggers/job_details.js';
+import candidateDetailsTrigger from './triggers/candidate_details';
+import newCandidateTrigger from './triggers/new_candidate';
+import allHiresTrigger from './triggers/all_hires');
+import updateHireCreate from './creates/update_hire_stage';
+import findHireSearch from './searches/find_hire';
+import allHiresSearch from './searches/all_hires.js';
+
 import Recipe from './resources/recipe';
 import { version } from '../package.json';
 import { version as platformVersion } from 'zapier-platform-core';
@@ -7,7 +18,7 @@ const App = {
   version,
   platformVersion,
 
-  authentication: Authentication,
+  authentication: authentication,
 
   beforeRequest: [],
 
@@ -17,11 +28,24 @@ const App = {
     [Recipe.key]: Recipe
   },
 
-  triggers: {},
+  triggers: {
+    [newHireTrigger.key]: newHireTrigger,
+    [hireDetailsTrigger.key]: hireDetailsTrigger,
+    [newJobsTrigger.key]: newJobsTrigger,
+    [jobDetailsTrigger.key]: jobDetailsTrigger,
+    [newCandidateTrigger.key]: newCandidateTrigger,
+    [candidateDetailsTrigger.key]: candidateDetailsTrigger,
+    [allHiresTrigger.key]: allHiresTrigger,
+  },
 
-  searches: {},
+  searches: {
+    [findHireSearch.key]: findHireSearch,
+    [allHiresSearch.key]: allHiresSearch,
+  },
 
-  creates: {}
+  creates: {
+    [updateHireCreate.key]: updateHireCreate
+  }
 };
 
 export default App;
